3a436dd2f006c2042340a0699fa1452312751f7f
gksktl111 <gksktl111@naver.com>
2023-11-14 18:32:37 +0900

    commit

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
+const { MongoClient, ObjectId } = require('mongodb');
+const methodOverride = require('method-override');
 
+app.use(methodOverride('_method'));
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded());
-
-const { MongoClient } = require('mongodb');
 
 let db;
 const url =
@@ -48,7 +49,7 @@ app.get('/news', (요청, 응답) => {
 });
 
 app.get('/list', async (요청, 응답) => {
-  let data = await db.collection('post').find().toArray();
+  let data = await db.collection('post').find().limit(5).toArray();
   응답.render('list.ejs', { posts: data });
 });
 
@@ -77,4 +78,83 @@ app.post('/newpost', async (요청, 응답) => {
 
 app.get('/time', (요청, 응답) => {
   응답.render('time.ejs', { date: new Date() });
+});
+
+app.get('/detail/:id', async (요청, 응답) => {
+  try {
+    // db의 해당 collection에서 맞는 document를 가져옴
+    const post = await db
+      .collection('post')
+      .findOne({ _id: new ObjectId(요청.params.id) });
+    // console.log(post);
+    // console.log(요청.params.id);
+
+    if (post == null) {
+      throw new Error();
+    }
+
+    응답.render('detail.ejs', { post: post });
+  } catch (error) {
+    console.log(error);
+    // 400클라문제, 500서버문제
+    응답.status(400).send('이상한 url 입력함');
+  }
+});
+
+app.get('/edit/:id', async (요청, 응답) => {
+  const post = await db
+    .collection('post')
+    .findOne({ _id: new ObjectId(요청.params.id) });
+
+  응답.render('edit.ejs', { post: post });
+});
+
+app.post('/edit/:id', async (요청, 응답) => {
+  try {
+    await db
+      .collection('post')
+      .updateOne(
+        { _id: new ObjectId(요청.params.id) },
+        { $set: { title: 요청.body.title, content: 요청.body.content } }
+      );
+
+    응답.redirect('/list');
+  } catch (error) {
+    응답.status(500).send(error);
+  }
+});
+
+app.put('/edit/:id', async (요청, 응답) => {
+  try {
+    await db
+      .collection('post')
+      .updateOne(
+        { _id: new ObjectId(요청.params.id) },
+        { $set: { title: 요청.body.title, content: 요청.body.content } }
+      );
+
+    응답.redirect('/list');
+  } catch (error) {
+    응답.status(500).send(error);
+  }
+});
+
+app.delete('/delete', async (요청, 응답) => {
+  try {
+    await db.collection('post').deleteOne({ _id: new ObjectId(요청.body.id) });
+    응답.sendStatus(200);
+  } catch (error) {
+    응답.sendStatus(500);
+  }
+});
+
+app.get('/list/:page', async (요청, 응답) => {
+  let data = await db
+    .collection('post')
+    .find()
+    // 스킵 성능 않좋은 너무 많은 데이터 관리에는 사용 X
+    .skip((요청.params.page - 1) * 5)
+    .limit(5)
+    .toArray();
+  응답.render('list.ejs', { posts: data });
 });
